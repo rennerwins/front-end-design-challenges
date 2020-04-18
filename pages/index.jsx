2818da3dabf9566f968301122b52fe50364f8909
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2020-04-18 21:25:51 +0700

    add deploy script

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import Title from '../src/components/Title/Title'
-import { Container, GlobalStyle } from './index.style'
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const Container = styled.div`
+	min-height: 100vh;
+`
 
 const Home = () => {
 	return (
